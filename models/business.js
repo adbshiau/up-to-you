31d83ae08fd252a26490847e9bee6f9574c9c89f
ayashiau-ga <angelicashiau@gmail.com>
2022-05-21 07:54:16 -0700

    favorites, reviews, and business model added to models

---
+++ models/business.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const reviewsSchema = mongoose.Schema({
+    username: String,
+    businessName: String,
+    userId: { type: mongoose.Schema.Types.ObjectId },
+    businessId: { type: mongoose.Schema.Types.ObjectId },
+    text: String,
+    stars: Number
+})
+
+const favoritesSchema = mongoose.Schema({
+    username: String,
+    userId: { type: mongoose.Schema.Types.ObjectId }
+})
+
+// A business has many favorites. A favorite belongs to a business.
+// A business has many reviews. A review belongs to a business.
+const businessSchema = new mongoose.Schema({
+    yelpId: String,
+    businessName: String,
+    yelpUrl: String,
+    imageUrl: String,
+    rating: Number,
+    price: String,
+    location: String,
+    phone: Number,
+    is_closed: Boolean,
+    // userFavorited: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    favorites: [favoritesSchema],
+    reviews: [reviewsSchema]
+})
+
+module.exports = mongoose.model('Business', businessSchema);
