bc609da2131a336674e08bd189f7cc48b8dde819
ayashiau-ga <angelicashiau@gmail.com>
2022-05-21 08:05:16 -0700

    business model updated

--- models/business.js
+++ models/business.js
@@ -1,34 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const reviewsSchema = mongoose.Schema({
+const reviewsSchema = mongoose.Schema(
+  {
     username: String,
     businessName: String,
     userId: { type: mongoose.Schema.Types.ObjectId },
     businessId: { type: mongoose.Schema.Types.ObjectId },
     text: String,
-    stars: Number
-})
+    stars: Number,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const favoritesSchema = mongoose.Schema({
-    username: String,
-    userId: { type: mongoose.Schema.Types.ObjectId }
-})
+  username: String,
+  userId: { type: mongoose.Schema.Types.ObjectId },
+});
 
 // A business has many favorites. A favorite belongs to a business.
 // A business has many reviews. A review belongs to a business.
 const businessSchema = new mongoose.Schema({
-    yelpId: String,
-    businessName: String,
-    yelpUrl: String,
-    imageUrl: String,
-    rating: Number,
-    price: String,
-    location: String,
-    phone: Number,
-    is_closed: Boolean,
-    // userFavorited: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    favorites: [favoritesSchema],
-    reviews: [reviewsSchema]
-})
+  yelpId: String,
+  businessName: String,
+  yelpUrl: String,
+  imageUrl: String,
+  rating: Number,
+  price: String,
+  location: String,
+  phone: Number,
+  is_closed: Boolean,
+  userFavorited: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  favorites: [favoritesSchema], // embedded the favorites schema
+  reviews: [reviewsSchema], // embedded the reviews schema
+});
 
-module.exports = mongoose.model('Business', businessSchema);
+module.exports = mongoose.model("Business", businessSchema);
