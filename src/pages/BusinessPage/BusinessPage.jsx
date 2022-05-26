34207d92fed4a5b9c619480a4272a9e6d1645b16
ayashiau-ga <angelicashiau@gmail.com>
2022-05-26 11:59:03 -0700

    business page updated

--- src/pages/BusinessPage/BusinessPage.jsx
+++ src/pages/BusinessPage/BusinessPage.jsx
@@ -1,14 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Grid } from "semantic-ui-react";
+import { Grid, Container } from "semantic-ui-react";
+import './BusinessPage.css'
 import PageHeader from "../../components/PageHeader/PageHeader";
 import SearchItem from "../../components/SearchItem/SearchItem";
 import AddReviewForm from "../../components/AddReviewForm/AddReviewForm";
 import ReviewsSection from "../../components/ReviewsSection/ReviewsSection";
 import * as reviewAPI from "../../utils/reviewApi";
 
-export default function BusinessPage({ user, handleLogout, business, showProfile }) {
-  
+export default function BusinessPage({
+  user,
+  handleLogout,
+  business,
+  showProfile,
+}) {
   const [reviews, setReviews] = useState(business.reviews);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
@@ -21,36 +26,34 @@ export default function BusinessPage({ user, handleLogout, business, showProfile
   async function handleAddReview(review) {
     setLoading(true);
     const data = await reviewAPI.create(review, business._id);
-    await setReviews(data.reviews)
+    await setReviews(data.reviews);
     showProfile(business._id);
     setLoading(false);
   }
 
-    useEffect(() => {
-    }, [reviews]);
+  useEffect(() => {}, [reviews]);
 
   return (
-    <Grid>
-      <Grid.Row>
-          <Grid.Column>
-          <PageHeader user={user} handleLogout={handleLogout} />
-          </Grid.Column>
-      </Grid.Row>
-      <Grid.Row>
-          <Grid.Column>
-              <SearchItem result={business} />
-          </Grid.Column>
-      </Grid.Row>
-      <Grid.Row>
-          <Grid.Column>
-                <AddReviewForm user={user} handleAddReview={handleAddReview} />
-          </Grid.Column>
-      </Grid.Row>
-      <Grid.Row>
-          <Grid.Column>
-                <ReviewsSection reviews={reviews} setReviews={setReviews}/>
-          </Grid.Column>
-      </Grid.Row>
-    </Grid>
+    <div>
+      <Container>
+        <PageHeader user={user} handleLogout={handleLogout} />
+      </Container>
+
+      <Container id='profile'style={{ marginTop: "7em" }}>
+        <Grid>
+          <Grid.Row>
+            <SearchItem result={business} />
+          </Grid.Row>
+
+          <Grid.Row>
+            <AddReviewForm user={user} handleAddReview={handleAddReview} />
+          </Grid.Row>
+
+          <Grid.Row>
+            <ReviewsSection reviews={reviews} setReviews={setReviews} />
+          </Grid.Row>
+        </Grid>
+      </Container>
+    </div>
   );
 }
