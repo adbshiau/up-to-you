714a514085bd00253da8789ebf75b98c9672516a
ayashiau-ga <angelicashiau@gmail.com>
2022-05-25 18:16:36 -0700

    randompage works

--- src/pages/RandomPage/RandomPage.jsx
+++ src/pages/RandomPage/RandomPage.jsx
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import { Grid, Image, Card } from "semantic-ui-react";
 import PageHeader from "../../components/PageHeader/PageHeader";
 import SearchField from "../../components/SearchField/SearchField";
 import SearchResults from "../../components/SearchResults/SearchResults";
+import yelpService from "../../utils/yelpService";
 
-export default function RandomPage({ user, handleLogout, handleRandomSearch, business }) {
-  return (
+export default function RandomPage({ user, handleLogout }) {
+    
+    const [result, setResult] = useState()
+    
+    async function handleRandomSearch(data) {
+        const result = await yelpService.randomSearch(data);
+        console.log(result, 'handleRandomSearch')
+        setResult(result);
+    }
+    if (result) {
+        return (
+            <>
+              <Grid>
+                <Grid.Row>
+                  <Grid.Column>
+                    <PageHeader user={user} handleLogout={handleLogout} />
+                  </Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                    <Grid.Column>
+                    <h1>Random Page</h1>
+                    </Grid.Column>
+                </Grid.Row>
+              </Grid>
+              <h1>Random Page</h1>
+              <SearchField handleSearch={handleRandomSearch}/>
+                <Image src={result.image_url} wrapped ui={false} size='tiny' />
+                <Card.Content>
+                
+                    <Card.Header content={result.name}/>
+                    <Card.Meta>{result.price}</Card.Meta>
+                    <Card.Description>{result.location.display_address}</Card.Description>
+                
+                </Card.Content>
+            </>
+        )
+    }     
+    return (
     <>
       <Grid>
         <Grid.Row>
@@ -22,14 +59,6 @@ export default function RandomPage({ user, handleLogout, handleRandomSearch, bus
       </Grid>
       <h1>Random Page</h1>
       <SearchField handleSearch={handleRandomSearch}/>
-        {/* <Image src={business.image_url} wrapped ui={false} size='tiny' />
-        <Card.Content>
-        
-            <Card.Header content={business.name}/>
-            <Card.Meta>{business.price}</Card.Meta>
-            <Card.Description>{business.location.display_address}</Card.Description>
-        
-        </Card.Content> */}
     </>
   );
 }
