d4b656ceefcaf453bcc1fd3070cff8392463b929
ayashiau-ga <angelicashiau@gmail.com>
2022-08-12 14:35:34 -0700

    right margin gap fixed

--- src/components/PageHeader/PageHeader.jsx
+++ src/components/PageHeader/PageHeader.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Dropdown, Icon, Menu, Container, Image } from "semantic-ui-react";
+import { Dropdown, Icon, Menu, Image } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import "./PageHeader.css";
 
@@ -7,6 +7,7 @@ export default function PageHeader({ user, handleLogout }) {
 
   const [click, setClick] = useState(false);
   const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
   const style = {
     backgroundColor: "#82ace6",
@@ -17,7 +18,7 @@ export default function PageHeader({ user, handleLogout }) {
   return (
       <Menu fixed="top" inverted borderless style={style} className={click ? 'active' : ''}>
         <div className="menu-icon" onClick={handleClick}>
-            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+            <Icon name={click ? "close" : "bars"} />
         </div>
         <Menu.Item as="a" header>
           <Image
@@ -25,17 +26,17 @@ export default function PageHeader({ user, handleLogout }) {
             style={{ marginRight: "1em" }}
             src="https://i.pinimg.com/originals/3e/c7/c4/3ec7c4ce95243e371f7e7469bdc5a518.png"
           />
-          <Link to="/home">Up To You</Link>
+          <Link to="/home" onClick={closeMobileMenu}>Up To You</Link>
         </Menu.Item>
         <Menu.Menu className={click ? 'active' : ''}>
         <Menu.Item>
-          <Link to="/random">Random</Link>
+          <Link to="/random" onClick={closeMobileMenu}>Random</Link>
         </Menu.Item>
         <Menu.Item>
-          <Link to="/search">Search</Link>
+          <Link to="/search" onClick={closeMobileMenu}>Search</Link>
         </Menu.Item>
         <Menu.Item>
-          <Link to="/history">History</Link>
+          <Link to="/history" onClick={closeMobileMenu}>History</Link>
         </Menu.Item>
         <Menu.Item>
           <Image
@@ -49,7 +50,7 @@ export default function PageHeader({ user, handleLogout }) {
         <Dropdown item simple text={user.username} floating="right">
           <Dropdown.Menu>
             <Dropdown.Item>
-              <Link to={`/${user?.username}`} style={{ color: "black" }}>
+              <Link to={`/${user?.username}`} style={{ color: "black" }} onClick={closeMobileMenu}>
                 Profile Page
               </Link>
             </Dropdown.Item>
